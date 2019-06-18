88439fb98d89c1f057faf939100d05ab898dc02d
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-18 18:21:41 +0100

    Setting up components

---
+++ src/components/From.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div> 
+        <h3> This is form component </h3>
+      </div>
+    )
+  }
+}
+
+export default Form
---
+++ src/components/Weater.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Weater extends React.Component {
+  render() {
+    return (
+      <div> 
+      <h2> This is the wetaher info </h2>
+      </div>
+    )
+  }
+}
+
+export default Weater 
